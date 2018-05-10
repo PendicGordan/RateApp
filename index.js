960554fb7d18a15f1c9ee59294a67dbaa3cca8eb
Gordan Pendic <gordan.pendic5@gmail>
2018-05-10 19:30:08 +0200

    Second commit

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ const httpsOptions = {
 };
 
 https.createServer(httpsOptions, app).listen(3000, function() {
-  console.log('Listening on port 3000');
+  console.log('Listening on port 3000'); 
 });
 
 /*app.listen(3000, function() {
